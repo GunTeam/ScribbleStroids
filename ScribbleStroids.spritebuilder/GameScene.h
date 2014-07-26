e41a2e1081bc9498b0f434d543804f38cd6311a6
Jorrie Brettin <jorrieb@mit.edu>
2014-07-26 13:32:15 -0400

    Completed main menu
    Changed project orientation
    Added three buttons
    Linked Buttons and title label to code

---
+++ ScribbleStroids.spritebuilder/GameScene.h
@@ -0,0 +1,16 @@
+//
+//  GameScene.h
+//  ScribbleStroids
+//
+//  Created by Jorrie Brettin on 7/26/14.
+//  Copyright 2014 Apportable. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface GameScene : CCNode {
+    
+}
+
+@end
--- ScribbleStroids.spritebuilder/Source/MainScene.h
+++ ScribbleStroids.spritebuilder/Source/MainScene.h
@@ -6,8 +6,16 @@
 //  Copyright (c) 2013 Apportable. All rights reserved.
 //
 
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
 #import "CCNode.h"
 
 @interface MainScene : CCNode
+{
+    CCLabelTTF *_titleLabel;
+}
+-(void) playGame;
+-(void) howTo;
+-(void) highScores;
 
 @end
