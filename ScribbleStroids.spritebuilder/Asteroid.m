69a6856b963624dc5641a3c02b2b5fcc8b58c42d
Jorrie Brettin <jorrieb@mit.edu>
2014-07-29 14:27:55 -0400

    Asteroid implementation
    Implemented asteroid class
    Overwrote the update function for asteroid

--- ScribbleStroids.spritebuilder/Asteroid.m
+++ ScribbleStroids.spritebuilder/Asteroid.m
@@ -5,10 +5,39 @@
 //  Created by Jorrie Brettin on 7/28/14.
 //  Copyright 2014 Apportable. All rights reserved.
 //
+// This class takes care of all the asteroids
 
 #import "Asteroid.h"
 
 
 @implementation Asteroid
+
+-(void) didLoadFromCCB {
+    
+    CGRect screenBound = [[UIScreen mainScreen] bounds];
+    CGSize screenSize = screenBound.size;
+    screenWidth = screenSize.width;
+    screenHeight = screenSize.height;
+}
+
+-(void) update:(CCTime)delta {
+    
+    if (self.position.x > screenWidth) {
+        CCLOG(@"it thinks position is greater than width");
+        self.position = CGPointMake(0, self.position.y);
+    } else if (self.position.x < 0) {
+        CCLOG(@"it thinks position is less than 0");
+        self.position = CGPointMake(screenWidth, self.position.y);
+    }
+    
+    if (self.position.y > screenHeight) {
+        CCLOG(@"it thinks position is greater than height");
+        self.position = CGPointMake(self.position.x, 0);
+    } else if (self.position.y < 0) {
+        CCLOG(@"it thinks y is less than 0");
+        self.position = CGPointMake(self.position.x, screenHeight);
+    }
+}
+
 
 @end
--- ScribbleStroids.spritebuilder/GameScene.m
+++ ScribbleStroids.spritebuilder/GameScene.m
@@ -20,9 +20,18 @@ double missileLaunchImpulse = 3;
     screenWidth = screenSize.width;
     screenHeight = screenSize.height;
     
+    Asteroid *asteroid = (Asteroid *) [CCBReader load:@"Asteroid"];
+    //you can't assign its position here because it thinks it starts at scale 1, pos (0,0)
+    asteroid.position = CGPointMake(100, 300);
+    asteroid.scale = .4;
+    asteroid.physicsBody.collisionType = @"asteroid";
+    [_physicsNode addChild:asteroid];
+    
     mainShip = (Ship *)[CCBReader load:@"Ship"];
     mainShip.position = CGPointMake(100, 100);
     mainShip.scale = .2;
+    mainShip.physicsBody.collisionGroup = @"ShipGroup";
+    mainShip.physicsBody.collisionType = @"ship";
     [_physicsNode addChild:mainShip z:2];
     _physicsNode.debugDraw = true;
 
@@ -37,16 +46,17 @@ double missileLaunchImpulse = 3;
         mainShip.rotation += 5;
     }
     
-    if (_boostButton.state) {
-//        mainShip.physicsBody.velocity = CGPointMake(-75, -75);
-        
+    if (_boostButton.state) {        
         CGFloat shipDirection = mainShip.rotation;
         CGPoint thrust = CGPointMake(15*cos(shipDirection*M_PI/180), 15*-sin(shipDirection*M_PI/180));
         [mainShip.physicsBody applyImpulse:thrust];
+        
     } else {
         mainShip.physicsBody.velocity = CGPointMake(mainShip.physicsBody.velocity.x*.995,
                                                     mainShip.physicsBody.velocity.y*.995);
     }
+    mainShip.physicsBody.angularVelocity = mainShip.physicsBody.angularVelocity*.995;
+
     
     if (mainShip.position.x > screenWidth){
         mainShip.position = CGPointMake(0, mainShip.position.y);
@@ -60,6 +70,13 @@ double missileLaunchImpulse = 3;
         mainShip.position = CGPointMake(mainShip.position.x, screenHeight);
     }
     
+}
+
+-(void) addAsteroid:(int)typeOfAsteroid{
+    if (typeOfAsteroid == 0) {
+        [_physicsNode addChild:[CCBReader load:@"Asteroid"]];
+        [_physicsNode addChild:[CCBReader load:@"Asteroid"]];
+    }
 }
 
 -(void) TurnLeft{
@@ -80,12 +97,19 @@ double missileLaunchImpulse = 3;
     CCSprite *bullet = (CCSprite *)[CCBReader load:@"Bullet"];
     
     bullet.position = mainShip.position;
-    
+    bullet.physicsBody.collisionGroup = @"ShipGroup";
+    bullet.physicsBody.collisionType = @"bullet";
     bullet.physicsBody.velocity = mainShip.physicsBody.velocity;
     [_physicsNode addChild:bullet z:1];
     CGFloat shipDirection = mainShip.rotation;
     [bullet.physicsBody applyImpulse: CGPointMake(missileLaunchImpulse*cos(shipDirection*M_PI/180),
                                                   missileLaunchImpulse*-sin(shipDirection*M_PI/180))];
+}
+
+- (BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair bullet:(CCNode *)bullet asteroid:(Asteroid *)asteroid {
+    // collision handling
+    CCLOG(@"Asteroid and bullet collided");
+    return true;
 }
 
 @end
