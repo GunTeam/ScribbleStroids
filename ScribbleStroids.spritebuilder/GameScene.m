a209de9bdaa617c817d5131684021db23c20f744
Jorrie Brettin <jorrieb@mit.edu>
2014-07-29 21:10:18 -0400

    Commented code thoroghly
    Added comments mainly on the GameScene
    Switched the modulus positioning method from the game scene into the
    Ship class

--- ScribbleStroids.spritebuilder/GameScene.m
+++ ScribbleStroids.spritebuilder/GameScene.m
@@ -15,11 +15,14 @@ double missileLaunchImpulse = 3;
 
 -(void) didLoadFromCCB {
     
+    //This block enables multi-touch
+    //This should not need to be touched
     [[[CCDirector sharedDirector] view] setMultipleTouchEnabled:YES];
     _leftButton.exclusiveTouch = false;
     _rightButton.exclusiveTouch = false;
     _boostButton.exclusiveTouch = false;
     _shootButton.exclusiveTouch = false;
+    //End multi-touch block
     
     CGRect screenBound = [[UIScreen mainScreen] bounds];
     CGSize screenSize = screenBound.size;
@@ -30,26 +33,40 @@ double missileLaunchImpulse = 3;
     _physicsNode = [[CCPhysicsNode alloc]init];
     [self addChild:_physicsNode z:-1];
     
+    //this is needed so physics methods that happen on the physics node
+    //can be handled by the gamescene
     _physicsNode.collisionDelegate = self;
     
-    //
-    
+    //this is how an asteroid should be spawned.
+    //You can set the initial velocity using random numbers
+    //The asteroid scale doesn't really matter at this point, it will change when we get
+    //new artwork.
+    //You must induce the physicsBody property of an object to access properties like
+    //velocity and angularVelocity, and to access methods like applyImpulse
     Asteroid *asteroid = (Asteroid *) [CCBReader load:@"Asteroid"];
-    
     asteroid.position = CGPointMake(100, 300);
+    asteroid.physicsBody.velocity = CGPointMake(10, 10);
     asteroid.scale = .4;
-    
     [_physicsNode addChild:asteroid z:-10];
     
+    
+    //spawn the ship
+    //The ship's scale also doesn't actually matter at this point. We need
+    //to get the artwork to know just what the scale will be. We place it in the
+    //center of the screen.
     mainShip = (Ship *)[CCBReader load:@"Ship"];
-    mainShip.position = CGPointMake(100, 100);
+    mainShip.position = CGPointMake(screenWidth/2, screenHeight/2);
     mainShip.scale = .2;
-    [_physicsNode addChild:mainShip z:-1];
+    [_physicsNode addChild:mainShip];
+    
+    //this is for debugging purposes. While true, you can see the physics bodies
     _physicsNode.debugDraw = true;
 
 }
 
+//The update function is called 60 times per second and is automatically called
 -(void) update:(CCTime)delta{
+    //_button.state is true when the button is pressed.
     if (_leftButton.state) {
         mainShip.rotation -=5;
         mainShip.physicsBody.angularVelocity = 0;
@@ -66,25 +83,17 @@ double missileLaunchImpulse = 3;
         [mainShip.physicsBody applyImpulse:thrust];
         
     } else {
+        //this is the dampening part of the ship. When you're not touching the thrusters,
+        //it slowly comes to a stop.
         mainShip.physicsBody.velocity = CGPointMake(mainShip.physicsBody.velocity.x*.995,
                                                     mainShip.physicsBody.velocity.y*.995);
     }
     mainShip.physicsBody.angularVelocity = mainShip.physicsBody.angularVelocity*.995;
-
-    
-    if (mainShip.position.x > screenWidth){
-        mainShip.position = CGPointMake(0, mainShip.position.y);
-    } else if (mainShip.position.x < 0){
-        mainShip.position = CGPointMake(screenWidth, mainShip.position.y);
-    }
-    
-    if (mainShip.position.y > screenHeight){
-        mainShip.position = CGPointMake(mainShip.position.x, 0);
-    } else if (mainShip.position.y < 0) {
-        mainShip.position = CGPointMake(mainShip.position.x, screenHeight);
-    }
     
 }
+
+//These methods are called by touchUpInside actions by the four buttons
+//on screen. I don't know how to change it so it's not touchUpInside.
 
 -(void) TurnLeft{
     CCLOG(@"Left Button Pressed");
@@ -107,14 +116,14 @@ double missileLaunchImpulse = 3;
     // collision handling
     CCLOG(@"Asteroid and ship collided");
     
-    
-    
     return true;
 }
 
 - (BOOL)ccPhysicsCollisionBegin:(CCPhysicsCollisionPair *)pair bullet:(CCSprite *)bullet asteroid:(Asteroid *)asteroid {
     // collision handling
     CCLOG(@"Asteroid and bullet collided");
+    
+    //this splits the asteroid that is being shot in half if it's large enough
     
     if (asteroid.scale > .11) {
         
@@ -126,7 +135,6 @@ double missileLaunchImpulse = 3;
         asteroid1.physicsBody.velocity = CGPointMake(rx1, ry1);
         [_physicsNode addChild:asteroid1];
         
-        
         Asteroid *asteroid2 = (Asteroid *)[CCBReader load:@"Asteroid"];
         asteroid2.position = asteroid.position;
         asteroid2.scale = asteroid.scale*.5;
@@ -135,9 +143,8 @@ double missileLaunchImpulse = 3;
         asteroid2.physicsBody.velocity = CGPointMake(rx2, ry2);
         [_physicsNode addChild:asteroid2];
         
-        
     }
-    
+    //destroy the asteroid and the bullet
     [asteroid removeFromParent];
     [bullet removeFromParent];
     
--- ScribbleStroids.spritebuilder/Ship.m
+++ ScribbleStroids.spritebuilder/Ship.m
@@ -15,8 +15,29 @@ double bulletLaunchImpulse = 3;
 -(void) didLoadFromCCB {
     self.physicsBody.collisionType = @"ship";
     self.physicsBody.collisionGroup = @"ShipGroup";
+    
+    CGRect screenBound = [[UIScreen mainScreen] bounds];
+    CGSize screenSize = screenBound.size;
+    screenWidth = screenSize.width;
+    screenHeight = screenSize.height;
 }
 
+-(void) update:(CCTime)delta{
+    //The ship knows where it is on screen and can roll over to the other side accordingly
+    if (self.position.x > screenWidth){
+        self.position = CGPointMake(0, self.position.y);
+    } else if (self.position.x < 0){
+        self.position = CGPointMake(screenWidth, self.position.y);
+    }
+    
+    if (self.position.y > screenHeight){
+        self.position = CGPointMake(self.position.x, 0);
+    } else if (self.position.y < 0) {
+        self.position = CGPointMake(self.position.x, screenHeight);
+    }
+}
+
+//This is the method that fires the bullets.
 -(void) fire {
     CCLOG(@"Ship has fired");
     CCSprite *bullet = (CCSprite *)[CCBReader load:@"Bullet"];
