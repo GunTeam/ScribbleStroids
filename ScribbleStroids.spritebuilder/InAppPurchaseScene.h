ae904cc8961178c54b17519bc1e4f6ce1d317d47
Jorrie Brettin <jorrieb@mit.edu>
2014-09-17 01:13:58 -0400

    Added inapp purchase file
    Added file sent by adam

--- ScribbleStroids.spritebuilder/InAppPurchaseScene.h
+++ ScribbleStroids.spritebuilder/InAppPurchaseScene.h
@@ -1,21 +1,27 @@
 //
-//  InAppPurchaseScene.h
-//  ScribbleStroids
+//  InAppPurchaseScene.h
+//  ScribbleStroids
 //
-//  Created by Jorrie Brettin on 9/16/14.
-//  Copyright 2014 Apportable. All rights reserved.
+//  Created by Jorrie Brettin on 9/16/14.
+//  Copyright 2014 Apportable. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
+#import <StoreKit/StoreKit.h>
 #import "cocos2d.h"
 #import "Stars.h"
 
-@interface InAppPurchaseScene : CCNode {
+@interface InAppPurchaseScene : CCNode <SKPaymentTransactionObserver, SKProductsRequestDelegate> {
     double SSS,MSS,LSS;
     NSMutableArray *smallStarsArray,*mediumStarsArray,*largeStarsArray;
     double smallStarSpeedX,smallStarSpeedY,mediumStarSpeedX,mediumStarSpeedY,largeStarSpeedX,largeStarSpeedY;
     CGFloat screenWidth,screenHeight;
     
+    int numberOfAdditionalCoins;
+    
 }
+
+@property (strong, nonatomic) SKProduct *product;
+@property (strong, nonatomic) NSString *productID;
 
 @end
